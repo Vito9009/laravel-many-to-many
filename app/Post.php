de2aac73853f5615c9a6909d15f8622b6e5f7222
Your Name <yan.vi193@gmail.com>
2022-03-21 15:14:09 +0100

    first commit

---
+++ app/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = ['title','content','slug', 'category_id', 'image'];
+
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
+}
